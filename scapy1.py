0cd1bb47bf6665378372ddbeec0fc638bdd97747
jungdaan <gong3gong@gmail.com>
2016-10-12 13:33:38 +0000

    Modified scapy1.py

--- scapy1.py
+++ scapy1.py
@@ -15,48 +15,52 @@ kakao_re = re.compile("http://(th-)?p.talk.kakao.co.kr")
 remove_duplicate = {} # remove duplicate requests
 
 def http_header(packet):
-    
-    str_pkt = str(packet)
-    #a = GET_re.findall("GET th/talkp/wksCVVLDGd/EPc2iXgCMBCg0S75Be6S80/1obrnl_940x940_s.jpg HTTP/1.1\r\n")
+    if packet.haslayer("Dot11Beacon"):
+        return
+    elif packet.haslayer("TCP") == 0:
+        return
+
+    str_pkt = packet.decode('utf-8')
+   #packet.display()
+#a = GET_re.findall("GET th/talkp/wksCVVLDGd/EPc2iXgCMBCg0S75Be6S80/1obrnl_940x940_s.jpg HTTP/1.1\r\n")
     ##b = HOST_re.findall("Host: http://p.talk.kakao.co.kr\x0d\x0a")
     #print a.group(0)
     #print b.group(0)
     matched_GET = GET_re.findall(str_pkt)
     matched_HOST = HOST_re.findall(str_pkt)
-    
+
     global remove_duplicate
 
     if len(matched_GET) != 0 and len(matched_HOST) != 0:
         matched_url2 = matched_GET[0]
         matched_url1 = matched_HOST[0][0]
-                                                                                                                                                                                                              
+                                                                                                                                                                
         full_url = matched_url1 + matched_url2
 
         if full_url not in remove_duplicate.keys():
             remove_duplicate[full_url] = 1
-            
+
             fName_idx = full_url.rfind("/")
             filename = full_url[ fName_idx+1 : ]
-            print full_url
+            print(full_url)
             download_kakao_jpg(full_url, filename)
-            
+
     else:
         pass
         #print "No KaKao"
 
 
 def download_kakao_jpg(full_url, filename):
-
-
+    print("download kakao jpg")
     if full_url.find("http://") == -1:
         full_url = "http://" + full_url
 
     requester = requests.get(full_url);
-    
+
     with open("./" + filename, "wb") as f:
         f.write(requester.content)
-    print "Success"
-    
+    print("Success")
+
 """
 def GET_print(packet1):
         http_GET_packet = str(packet1)
@@ -77,13 +81,15 @@ def GET_print(packet1):
 """
 
 if __name__ == '__main__':
-	mon_iface = ""
-	if len(sys.argv) != 2 :
-		print("USAGE : thiscode.py 'interface' ")
-		sys.exit()
-	mon_iface = sys.argv[1]
+        mon_iface = ""
+        if len(sys.argv) != 2 :
+                print("USAGE : thiscode.py 'interface' ")
+                sys.exit()
+        mon_iface = sys.argv[1]
 
-	try:
-		sniff(iface= "wlan0", prn=http_header, filter="tcp port 80")
-	except KeyboardInterrupt:
-		sys.exit()
+        try:
+                sniff(iface= mon_iface, prn=http_header)
+        except KeyboardInterrupt:
+                sys.exit()
+
+
