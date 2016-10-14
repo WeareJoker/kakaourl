1151fed76fcefe3d7770d50c6256e797507064d3
jungdaan <gong3gong@gmail.com>
2016-10-14 18:09:43 +0000

    update

--- scapy1.py
+++ scapy1.py
@@ -5,7 +5,7 @@ import re
 import sqlite3
 import requests
 import sys
-
+import exceptions
 
 GET_re = re.compile("GET ([a-zA-Z0-9\/+?/._]+) HTTP/1.1\r\n")
 HOST_re = re.compile("Host: ((http://)?(th-)?p.talk.kakao.co.kr)\r\n")
@@ -16,17 +16,14 @@ remove_duplicate = {} # remove duplicate requests
 
 def http_header(packet):
     str_pkt = str(packet)
-<<<<<<< HEAD
-=======
-    #a = GET_re.findall("GET th/talkp/wksCVVLDGd/EPc2iXgCMBCg0S75Be6S80/1obrnl_940x940_s.jpg HTTP/1.1\r\n")
->>>>>>> 2c280faf51c3f62e2b447c6fc4c748a17a315f09
+
     if packet.haslayer("Dot11Beacon"):
         return
     elif packet.haslayer("TCP") == 0:
         return
 
     str_pkt = str(packet)
-    print(repr(packet))
+    #print(repr(packet))
     matched_GET = GET_re.findall(str_pkt)
     matched_HOST = HOST_re.findall(str_pkt)
     
@@ -43,11 +40,11 @@ def http_header(packet):
             
             fName_idx = full_url.rfind("/")
             filename = full_url[ fName_idx+1 : ]
-<<<<<<< HEAD
+
             #print full_url
-=======
+
             print full_url
->>>>>>> 2c280faf51c3f62e2b447c6fc4c748a17a315f09
+
             download_kakao_jpg(full_url, filename)
             
     else:
@@ -60,29 +57,20 @@ def download_kakao_jpg(full_url, filename):
     
     if full_url.find("http://") == -1:
         full_url = "http://" + full_url
-<<<<<<< HEAD
+
     try:	
-    	requester = requests.get(full_url);
+    	requester = requests.get(full_url)
+        with open("./" + filename, "wb") as f:
+           f.write(requester.content)
+        with open("daankao.txt", "a+t") as f:
+           f.write(full_url + " " + filename + "\n")    
+        print "Success"
     except:
-	print "i need wifi"   
-=======
-		
-
-
-    requester = requests.get(full_url);
-       
->>>>>>> 2c280faf51c3f62e2b447c6fc4c748a17a315f09
-    with open("./" + filename, "wb") as f:
-        f.write(requester.content)
-    with open("daankao.txt", "a+t") as f:
-        f.write(full_url + " " + filename + "\n")
-
-    
-    print "Success"
+        print "internet not connected"
     
              
 
-<<<<<<< HEAD
+
 if __name__ == '__main__':
    if (len(sys.argv) != 2):
       if sys.argv[1] not in ["live", "pcap"]:
@@ -103,13 +91,13 @@ if __name__ == '__main__':
 
          #sniff(iface="tap0", prn=http_header, filter="tcp port 80")
 
-=======
+
 if len(sys.argv) != 3:
     print("USAGE : %s -p" % sys.argv[0]) 
 
 else :
 	pcap_file = "argv[1]"
-	pxap = rdpcap(pcap_file)
+	pcap = rdpcap(pcap_file)
 	data = ""
 	for packet in pcap:
 		http_header(packet)	
@@ -117,5 +105,5 @@ else :
 with open("daankao.txt", "wt") as f:
 	f.write("This is FULL_URL FILE_NAME\n")
 sniff(iface= "wlan0", prn=http_header, filter="tcp port 80")
->>>>>>> 2c280faf51c3f62e2b447c6fc4c748a17a315f09
+
 
