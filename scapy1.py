1df3246428b6baf53e7dc8434d23345b3b78c87b
jungdaan <gong3gong@gmail.com>
2016-10-14 12:50:44 +0000

    daani

--- scapy1.py
+++ scapy1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from scapy.all import *
 import re
 import sqlite3
@@ -15,81 +16,67 @@ kakao_re = re.compile("http://(th-)?p.talk.kakao.co.kr")
 remove_duplicate = {} # remove duplicate requests
 
 def http_header(packet):
-    if packet.haslayer("Dot11Beacon"):
-        return
-    elif packet.haslayer("TCP") == 0:
-        return
-
-    str_pkt = packet.decode('utf-8')
-   #packet.display()
-#a = GET_re.findall("GET th/talkp/wksCVVLDGd/EPc2iXgCMBCg0S75Be6S80/1obrnl_940x940_s.jpg HTTP/1.1\r\n")
+    
+    str_pkt = str(packet)
+    #a = GET_re.findall("GET th/talkp/wksCVVLDGd/EPc2iXgCMBCg0S75Be6S80/1obrnl_940x940_s.jpg HTTP/1.1\r\n")
     ##b = HOST_re.findall("Host: http://p.talk.kakao.co.kr\x0d\x0a")
     #print a.group(0)
     #print b.group(0)
     matched_GET = GET_re.findall(str_pkt)
     matched_HOST = HOST_re.findall(str_pkt)
-
+    
     global remove_duplicate
 
     if len(matched_GET) != 0 and len(matched_HOST) != 0:
         matched_url2 = matched_GET[0]
         matched_url1 = matched_HOST[0][0]
-                                                                                                                                                                
+                                                                                                                                                                                                              
         full_url = matched_url1 + matched_url2
 
         if full_url not in remove_duplicate.keys():
             remove_duplicate[full_url] = 1
-
+            
             fName_idx = full_url.rfind("/")
             filename = full_url[ fName_idx+1 : ]
-            print(full_url)
+            print full_url
             download_kakao_jpg(full_url, filename)
-
+            
     else:
         pass
         #print "No KaKao"
 
 
 def download_kakao_jpg(full_url, filename):
-    print("download kakao jpg")
+
+    
     if full_url.find("http://") == -1:
         full_url = "http://" + full_url
+		
+
 
     requester = requests.get(full_url);
-
+       
     with open("./" + filename, "wb") as f:
         f.write(requester.content)
-    print("Success")
+    with open("daankao.txt", "a+t") as f:
+        f.write(full_url + " " + filename + "\n")
 
-"""
-def GET_print(packet1):
-        http_GET_packet = str(packet1)
-        http_get_packet_list = http_GET_packet.split("\r\n")
-        for extrect_packet_line in http_get_packet_list:
-                http_packet_host_len = extrect_packet_line.find('Host:')
-                if -1 != http_packet_host_len:
-                    m = p.match(extrect_packet_line[http_packet_host_len+5:].strip())
-                    if m is not None:
-                        saveurl = m.group()
-                        print saveurl
-                        cursor = con.cursor()
-                        cursor.execute("insert into kakaourl (url) values (\'" + saveurl + "\');")
-                        con.commit()
-                    else:
-                        print('No Kakao')
+    
+    print "Success"
+    
+             
 
-"""
+if len(sys.argv) != 3:
+    print("USAGE : %s -p" % sys.argv[0]) 
 
-if __name__ == '__main__':
-        mon_iface = ""
-        if len(sys.argv) != 2 :
-                print("USAGE : thiscode.py 'interface' ")
-                sys.exit()
-        mon_iface = sys.argv[1]
+else :
+	pcap_file = "argv[1]"
+	pxap = rdpcap(pcap_file)
+	data = ""
+	for packet in pcap:
+		http_header(packet)	
 
-        try:
-                sniff(iface= mon_iface, prn=http_header)
-        except KeyboardInterrupt:
-                sys.exit()
-
+with open("daankao.txt", "wt") as f:
+	f.write("This is FULL_URL FILE_NAME\n")
+sniff(iface= "wlan0", prn=http_header, filter="tcp port 80")
 
